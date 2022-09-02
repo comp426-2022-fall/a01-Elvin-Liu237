5feac9155b089b6538dc5b548461db34d55382d0
Elvin Liu <elvinliu@live.unc.edu>
2022-09-02 14:10:03 -0400

    a

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const http = require('http')
 // Require fs module
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-///////const minimist = require('minimist');
+const minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-//const args = process.argv.slice(2);
-//const args = require('minimist')(process.argv.slice(2));
-var argv = require('minimist')(process.argv.slice(2));
+var argv = require(minimist)(process.argv.slice(2));
 console.log(argv);
+
+
+
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
