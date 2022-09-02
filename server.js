0a1208ebf938bcd91cd9eb1a933eb0d4b1601b07
Elvin Liu <elvinliu@live.unc.edu>
2022-09-02 13:30:57 -0400

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 //const args = process.argv.slice(2);
-const args = process.argv.slice(`--port=`);
+const args = require('minimist')(process.argv.slice(`--port=`));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
