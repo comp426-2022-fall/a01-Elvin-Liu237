9bf7fe208f51431c0642bd8c23ec6a750823293c
Elvin Liu <elvinliu@live.unc.edu>
2022-09-02 14:25:55 -0400

    dry

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const http = require('http')
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist');
+
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var argv = require(minimist)(process.argv.slice(2));
-console.log(argv);
-
-
+const args = require('minimist')(process.argv.slice(2));
 
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = process.argv.PORT || 5555
+
+const port = args.port || process.env.PORT || 5555
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
